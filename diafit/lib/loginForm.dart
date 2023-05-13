a987e0a0af3e9232b347b3b102b1debe331e6bc1
Nicholas Audric Surya <100661447+changjli@users.noreply.github.com>
2023-05-13 14:31:06 +0700

    coba2

---
+++ diafit/lib/loginForm.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+// Define a custom Form widget.
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  LoginFormState createState() {
+    return LoginFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class LoginFormState extends State<LoginForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'this field is required';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('submit'),
+          )
+        ],
+      ),
+    );
+  }
+}
